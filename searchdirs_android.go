645bff39dfe579582aabc0cb491a735479cac764
Mikk Margus Möll <mikk@you.die.ifyou.work>
2023-12-28 17:55:22 +0200

    update searchdirs

--- searchdirs_android.go
+++ searchdirs_android.go
@@ -2,6 +2,11 @@ package main
 
 func getSearchdirs(runpath []string) (ret []string) {
 	ret = append(ret, runpath...)
-	ret = append(ret, "/vendor/lib64", "/system/lib64", "/vendor/lib", "/system/lib")
+	// from https://android.googlesource.com/platform/bionic/+/refs/heads/main/linker/linker.cpp
+	ret = append(ret,
+		"/system/lib64", "/system/lib",
+		"/odm/lib64", "/odm/lib",
+		"/vendor/lib64", "/vendor/lib",
+	)
 	return ret
 }
--- searchdirs_linux.go
+++ searchdirs_linux.go
@@ -4,7 +4,12 @@ package main
 
 func getSearchdirs(runpath []string) (ret []string) {
 	ret = append(ret, runpath...)
-	ret = append(ret, "/lib", "/lib64", "/lib32", "/usr/lib", "/usr/lib64", "/usr/lib32")
+	// based on glibc and musl defaults
+	ret = append(ret,
+		"/lib64", "/lib",
+		"/usr/lib64", "/usr/lib",
+		"/usr/local/lib64", "/usr/local/lib",
+	)
 	ret = append(ret, parseLdSoConfFile("/etc/ld.so.conf", map[string]bool{})...)
 	return ret
 }
